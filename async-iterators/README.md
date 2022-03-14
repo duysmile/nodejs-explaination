6a30e93ef0ce9ced2ef3923262af2c79525d724f
Duy Nguyen <duy210697@gmail.com>
2022-03-15 00:32:15 +0700

    add tree map and basic logic in consistent hashing

---
+++ async-iterators/README.md
@@ -0,0 +1,3 @@
+# Async iterators
+
+This directory about how to use async iterators in some simple use cases
--- redis-ccu/test-script/test.js
+++ redis-ccu/test-script/test.js
@@ -6,8 +6,30 @@ export let options = {
     iterations: 100,
 };
 
-export default function() {
-    const res = http.post('http://localhost:3000/actions');
+export default function () {
+    const res = http.post('http://localhost:9000/app/game/start', JSON.stringify({
+        "game_id": "DCGF8B5bAJGFdZa4jrGA"
+    }), {
+        headers: {
+            'access_token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiRENDcFRycmFEOXRIR1pHelk0UE8ifQ.Fg3VbxH80OqvliMjsZtesPO_1boZtMbzAeWhOYrBQoU',
+            'Content-Type': 'application/json'
+        },
+    });
 
     check(res, { 'status was 200': (r) => r.status == 200 });
 }
+
+// export default function () {
+//     const res = http.post('http://119.82.141.119/api/v1/transcripts', JSON.stringify({
+//         "audioPath": "abc.wav",
+//         "channelId": "1",
+//         "transcript": "xin chào",
+//     }), {
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Authorization': 'wWPBZb7rKryrXLABP62cu2S6WqfSxcaQ',
+//         },
+//     });
+
+//     check(res, { 'status was 200': (r) => r.status == 200 });
+// }
